37e6239231237fcf6bb1d7fda175d2358d28eb27
nhandzai <ducnhan1522004@gmail.com>
2024-11-17 14:59:28 +0700

    add about us, contact us, login

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,43 @@ router.get('/catalog', function (req, res, next) {
   });
 });
 
+router.get('/home', function (req, res, next) {
+  ejs.renderFile(path.join(__dirname, '../views/home.ejs'), {}, function (err, str) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Home',
+      body: str, // Rendered home.ejs content
+    });
+  });
+});
+
+router.get('/about-us', function (req, res, next) {
+  ejs.renderFile(path.join(__dirname, '../views/about-us.ejs'), {}, function (err, str) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'About us',
+      body: str, // Rendered about-us.ejs content
+    });
+  });
+});
+
+router.get('/contact-us', function (req, res, next) {
+  ejs.renderFile(path.join(__dirname, '../views/contact-us.ejs'), {}, function (err, str) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Contact us',
+      body: str, // Rendered contact-us.ejs content
+    });
+  });
+});
+
+router.get('/sign-up', function (req, res, next) {
+  ejs.renderFile(path.join(__dirname, '../views/sign-up.ejs'), {}, function (err, str) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Sign up',
+      body: str, // Rendered sign-up.ejs content
+    });
+  });
+});
 module.exports = router;
