bb751863d9387db3b28f340f040d72eac163d2a5
PhamHoangKha1403 <phamhoangkha14032004@gmail.com>
2024-11-17 22:00:32 +0700

    BE update_V1

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,35 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var ejs = require('ejs');
+const db = require('../models');
+const renderProductHome = require('../views/product-home');
+const renderHome = require('../views/home');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  ejs.renderFile(path.join(__dirname, '../views/home.ejs'), {}, function (err, str) {
-    if (err) return next(err);
+
+
+
+// GET home page  
+
+router.get('/', async function (req, res, next) {
+  try {
+    // Lấy tất cả các sản phẩm từ cơ sở dữ liệu
+    const products = await db.products.findAll();
+
+    // Render các sản phẩm vào trang product-home.ejs
+    const productHomeStr = await renderProductHome(req, res, next, products);
+
+    // Render toàn bộ trang home
+    const str = await renderHome(req, res, next, productHomeStr);
+
+    // Trả về kết quả render với các dữ liệu đã được lấy từ cơ sở dữ liệu
     res.render('index', {
       title: 'Home',
       body: str, // Rendered home.ejs content
     });
-  });
+  } catch (error) {
+    next(error); // Xử lý lỗi nếu có
+  }
 });
-
 /* GET catalog page. */
 router.get('/catalog', function (req, res, next) {
   ejs.renderFile(path.join(__dirname, '../views/catalog.ejs'), {}, function (err, str) {
@@ -25,14 +42,20 @@ router.get('/catalog', function (req, res, next) {
   });
 });
 
-router.get('/home', function (req, res, next) {
-  ejs.renderFile(path.join(__dirname, '../views/home.ejs'), {}, function (err, str) {
-    if (err) return next(err);
+router.get('/home', async function (req, res, next) {
+  try {
+    const products = await db.products.findAll();
+
+    const productHomeStr = await renderProductHome(req, res, next, products);
+    const str = await renderHome(req, res, next, productHomeStr);
+
     res.render('index', {
       title: 'Home',
       body: str, // Rendered home.ejs content
     });
-  });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get('/about-us', function (req, res, next) {
