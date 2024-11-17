84845eaab8f871a1fc379a3683837cb41c7c164e
nhandzai <ducnhan1522004@gmail.com>
2024-11-17 13:06:32 +0700

    add home and catolog layout

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var ejs = require('ejs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',body: '' });
+router.get('/', function (req, res, next) {
+  ejs.renderFile(path.join(__dirname, '../views/home.ejs'), {}, function (err, str) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Home',
+      body: str, // Rendered home.ejs content
+    });
+  });
 });
 
 /* GET catalog page. */
-router.get('/catalog', function(req, res, next) {
-  res.render('catalog', { title: 'Catalog' });
+router.get('/catalog', function (req, res, next) {
+  ejs.renderFile(path.join(__dirname, '../views/catalog.ejs'), {}, function (err, str) {
+    if (err) return next(err);
+    res.render('index', {
+      title: 'Catalog',
+      body: str, // Rendered catalog.ejs content
+    });
+  });
 });
+
 module.exports = router;
